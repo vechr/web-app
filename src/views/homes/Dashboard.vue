e5520b81181dffaa8694f25b422d2a7e72d17882
zulfikar4568 <isnaen70@gmail.com>
2022-05-21 19:39:32 +0700

    :sparkles: leaflet

--- src/views/homes/Dashboard.vue
+++ src/views/homes/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
-  <a-row>
+
+    <a-row>
     <a-col :span="12">
       <a-button type="primary" @click="showDrawer"><PlusOutlined/>Widget</a-button>
     </a-col>
@@ -13,66 +14,77 @@
     style="color: red"
     title="Dashboard Widget"
     placement="right"
-    @after-visible-change="afterVisibleChange"
   >
   <div class="dashboard-panel">
-    <a-row >
-      <a-col :span="12">
-        <a-card @click="addNewBarChart()" align="middle">
-          <BarChartOutlined /><br/> Bar Chart
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-        <a-card @click="addNewLineChart()" align="middle">
-          <LineChartOutlined /><br/> Line Chart
-        </a-card>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="12">
-        <a-card @click="addNewBubbleChart()" align="middle">
-          <DotChartOutlined /><br/> Bubble Chart
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-        <a-card @click="addNewDoughnutChart()" align="middle">
-          <PieChartOutlined /><br/> Doughnut Chart
-        </a-card>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col @click="addNewPieChart()" :span="12">
-        <a-card align="middle">
-          <PieChartFilled /><br/> Pie Chart
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-        <a-card @click="addNewPolarAreaChart()" align="middle">
-          <PieChartTwoTone /><br/> Polar Area Chart
-        </a-card>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="12">
-        <a-card @click="addNewRadarChart()" align="middle">
-          <RadarChartOutlined /><br/> Radar Chart
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-        <a-card @click="addNewScatterChart()" align="middle">
-          <DotChartOutlined /><br/> Scatter Chart
-        </a-card>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="12">
-        <a-card @click="addNewGaugeChart()" align="middle">
-          <DashboardOutlined /><br/> Gauge
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-      </a-col>
-    </a-row>
+    <a-collapse v-model:activeKey="activeKey" ghost>
+      <a-collapse-panel key="1" header="Chart">
+        <a-row >
+          <a-col :span="12">
+            <a-card @click="addNewBarChart()" align="middle">
+              <BarChartOutlined /><br/> Bar Chart
+            </a-card>
+          </a-col>
+          <a-col :span="12">
+            <a-card @click="addNewLineChart()" align="middle">
+              <LineChartOutlined /><br/> Line Chart
+            </a-card>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-card @click="addNewBubbleChart()" align="middle">
+              <DotChartOutlined /><br/> Bubble Chart
+            </a-card>
+          </a-col>
+          <a-col :span="12">
+            <a-card @click="addNewDoughnutChart()" align="middle">
+              <PieChartOutlined /><br/> Doughnut Chart
+            </a-card>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col @click="addNewPieChart()" :span="12">
+            <a-card align="middle">
+              <PieChartFilled /><br/> Pie Chart
+            </a-card>
+          </a-col>
+          <a-col :span="12">
+            <a-card @click="addNewPolarAreaChart()" align="middle">
+              <PieChartTwoTone /><br/> Polar Area Chart
+            </a-card>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-card @click="addNewRadarChart()" align="middle">
+              <RadarChartOutlined /><br/> Radar Chart
+            </a-card>
+          </a-col>
+          <a-col :span="12">
+            <a-card @click="addNewScatterChart()" align="middle">
+              <DotChartOutlined /><br/> Scatter Chart
+            </a-card>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-card @click="addNewGaugeChart()" align="middle">
+              <DashboardOutlined /><br/> Gauge
+            </a-card>
+          </a-col>
+        </a-row>
+      </a-collapse-panel>
+      <a-collapse-panel key="2" header="Location">
+        <a-row>
+          <a-col :span="12">
+            <a-card @click="addNewMaps()" align="middle">
+              <EnvironmentOutlined /><br/> Maps
+            </a-card>
+          </a-col>
+        </a-row>
+      </a-collapse-panel>
+    </a-collapse>  
+
   </div>
   </a-drawer>
   <section class="grid-stack">
@@ -80,10 +92,11 @@
 </template>
 
 <script lang="ts">
-import { PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined, DashboardOutlined } from "@ant-design/icons-vue";
+import { EnvironmentOutlined, PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined, DashboardOutlined } from "@ant-design/icons-vue";
 import { defineComponent, onMounted, ref } from "vue";
 import { GridStack } from 'gridstack';
 import Chart from 'chart.js/auto';
+import L from "leaflet";
 import barChartData from '@/types/chart/bar-chart';
 import lineChartData from '@/types/chart/line-chart';
 import bubbleChartData from '@/types/chart/bubble-chart';
@@ -92,20 +105,19 @@ import polarAreaChartData from '@/types/chart/polar-chart';
 import radarChartData from '@/types/chart/radar-chart';
 import scatterChartData from '@/types/chart/scatter-chart';
 import gaugeChartData from '@/types/chart/gauge-chart';
+import { message } from "ant-design-vue";
 
 export default defineComponent({
   name: 'Dashboard',
-  components: { DashboardOutlined, PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined },
+  components: { EnvironmentOutlined, DashboardOutlined, PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined },
   setup () {
     const visible = ref<boolean>(false);
-
-    const afterVisibleChange = (bool: boolean) => {
-      console.log('visible', bool);
-    };
 
     const showDrawer = () => {
       visible.value = true;
     };
+
+    const activeKey = ref(['1', '2']);
 
     let count = ref(0);
     let info = ref("");
@@ -123,6 +135,47 @@ export default defineComponent({
         info.value = `you just dragged node #${node.id} to ${node.x},${node.y} – good job!`;
       });
     });
+
+    function addNewMaps() {
+      grid.compact();
+      const node: any = {
+        x: 12,
+        y: 5,
+        w: 12,
+        h: 5,
+      };
+      node.id = node.content = String(count.value++);
+      grid.addWidget('<div class="grid-stack-item event-map"><div class="grid-stack-item-content map-gridstack"><div class="container-map"><div class="my-map" id="map_' + node.id + '"></div></div></div></div>', node);
+      let map = new L.Map(`map_${node.id}`, {
+        center: new L.LatLng(40.731253, -73.996139),
+        zoom: 12,
+        attributionControl: false,
+        // dragging: false
+      });
+
+      map.locate({setView: true, maxZoom: 16});
+
+      map.on('locationfound', function (e) {
+        var radius = e.accuracy;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+      });
+
+      map.on('locationerror', function onLocationError(e) {
+          message.error(e.message);
+      });
+      
+      L.tileLayer(
+       "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+       {
+         attribution:'',
+         maxZoom: 18
+       }
+     ).addTo(map);
+    }
 
     function addNewGaugeChart() {
       grid.compact();
@@ -279,9 +332,10 @@ export default defineComponent({
       addNewRadarChart,
       addNewScatterChart,
       addNewGaugeChart,
+      addNewMaps,
       visible,
-      afterVisibleChange,
       showDrawer,
+      activeKey
     };
   }
 })
