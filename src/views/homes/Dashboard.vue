6f5c7ef164cba383543cc9b338d0d3977bc8de34
zulfikar4568 <isnaen70@gmail.com>
2022-08-07 15:21:23 +0700

    :zap: selection topic on widget

--- src/views/homes/Dashboard.vue
+++ src/views/homes/Dashboard.vue
@@ -1,7 +1,9 @@
 <template>
-    <a-row>
+  <a-row>
     <a-col :span="12">
-      <a-button type="primary" @click="showDrawer"><PlusOutlined/>Widget</a-button>
+      <a-button type="primary" @click="showDrawer"
+        ><PlusOutlined />Widget</a-button
+      >
     </a-col>
     <a-col style="text-align: right" :span="12">
       <h2 class="title-dashboard">Dashboard</h2>
@@ -14,109 +16,290 @@
     title="Dashboard Widget"
     placement="right"
   >
-  <div class="dashboard-panel">
-    <a-collapse v-model:activeKey="activeKey" ghost>
-      <a-collapse-panel key="1" header="Chart">
-        <a-row >
-          <a-col :span="12">
-            <a-card @click="addNewBarChart()" align="middle">
-              <BarChartOutlined /><br/> Bar Chart
-            </a-card>
-          </a-col>
-          <a-col :span="12">
-            <a-card @click="addNewLineChart()" align="middle">
-              <LineChartOutlined /><br/> Line Chart
-            </a-card>
-          </a-col>
-        </a-row>
-        <a-row>
-          <a-col :span="12">
-            <a-card @click="addNewBubbleChart()" align="middle">
-              <DotChartOutlined /><br/> Bubble Chart
-            </a-card>
-          </a-col>
-          <a-col :span="12">
-            <a-card @click="addNewDoughnutChart()" align="middle">
-              <PieChartOutlined /><br/> Doughnut Chart
-            </a-card>
-          </a-col>
-        </a-row>
-        <a-row>
-          <a-col @click="addNewPieChart()" :span="12">
-            <a-card align="middle">
-              <PieChartFilled /><br/> Pie Chart
-            </a-card>
-          </a-col>
-          <a-col :span="12">
-            <a-card @click="addNewPolarAreaChart()" align="middle">
-              <PieChartTwoTone /><br/> Polar Area Chart
-            </a-card>
-          </a-col>
-        </a-row>
-        <a-row>
-          <a-col :span="12">
-            <a-card @click="addNewRadarChart()" align="middle">
-              <RadarChartOutlined /><br/> Radar Chart
-            </a-card>
-          </a-col>
-          <a-col :span="12">
-            <a-card @click="addNewScatterChart()" align="middle">
-              <DotChartOutlined /><br/> Scatter Chart
-            </a-card>
-          </a-col>
-        </a-row>
-        <a-row>
-          <a-col :span="12">
-            <a-card @click="addNewGaugeChart()" align="middle">
-              <DashboardOutlined /><br/> Gauge
-            </a-card>
-          </a-col>
-        </a-row>
-      </a-collapse-panel>
-      <a-collapse-panel key="2" header="Location">
-        <a-row>
-          <a-col :span="12">
-            <a-card @click="addNewMaps()" align="middle">
-              <EnvironmentOutlined /><br/> Maps
-            </a-card>
-          </a-col>
-        </a-row>
-      </a-collapse-panel>
-    </a-collapse>  
+    <a-drawer
+      v-model:visible="configVisible"
+      :title="`${titleConfig} Configuration`"
+      width="320"
+      :closable="false"
+      @close="onConfigClose"
+    >
+      <a-form
+        layout="vertical"
+        :model="formState"
+        name="basic"
+        autocomplete="off"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+          label="Topic"
+          name="topicId"
+          :rules="[{ required: true, message: 'Please select Topic!' }]"
+        >
+          <a-select
+            v-model:value="formState.topicId"
+            placeholder="Select a Topic"
+            style="width: 100%"
+            :options="options"
+            @change="handleChange"
+            @focus="handleFocus"
+          ></a-select>
+        </a-form-item>
 
-  </div>
+        <a-form-item>
+          <a-button
+            type="primary"
+            html-type="submit"
+            :style="{ float: 'right' }"
+            >Create Widget</a-button
+          >
+        </a-form-item>
+      </a-form>
+    </a-drawer>
+    <div class="dashboard-panel">
+      <a-collapse v-model:activeKey="activeKey" ghost>
+        <a-collapse-panel key="1" header="Chart">
+          <a-row>
+            <a-col :span="12">
+              <a-card @click="addNewBarChart()" align="middle">
+                <BarChartOutlined /><br />
+                Bar Chart
+              </a-card>
+            </a-col>
+            <a-col :span="12">
+              <a-card @click="addNewLineChart()" align="middle">
+                <LineChartOutlined /><br />
+                Line Chart
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="12">
+              <a-card @click="addNewBubbleChart()" align="middle">
+                <DotChartOutlined /><br />
+                Bubble Chart
+              </a-card>
+            </a-col>
+            <a-col :span="12">
+              <a-card @click="addNewDoughnutChart()" align="middle">
+                <PieChartOutlined /><br />
+                Doughnut Chart
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col @click="addNewPieChart()" :span="12">
+              <a-card align="middle">
+                <PieChartFilled /><br />
+                Pie Chart
+              </a-card>
+            </a-col>
+            <a-col :span="12">
+              <a-card @click="addNewPolarAreaChart()" align="middle">
+                <PieChartTwoTone /><br />
+                Polar Area Chart
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="12">
+              <a-card @click="addNewRadarChart()" align="middle">
+                <RadarChartOutlined /><br />
+                Radar Chart
+              </a-card>
+            </a-col>
+            <a-col :span="12">
+              <a-card @click="addNewScatterChart()" align="middle">
+                <DotChartOutlined /><br />
+                Scatter Chart
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="12">
+              <a-card @click="addNewGaugeChart()" align="middle">
+                <DashboardOutlined /><br />
+                Gauge
+              </a-card>
+            </a-col>
+          </a-row>
+        </a-collapse-panel>
+        <a-collapse-panel key="2" header="Location">
+          <a-row>
+            <a-col :span="12">
+              <a-card @click="addNewMaps()" align="middle">
+                <EnvironmentOutlined /><br />
+                Maps
+              </a-card>
+            </a-col>
+          </a-row>
+        </a-collapse-panel>
+      </a-collapse>
+    </div>
   </a-drawer>
-  <section class="grid-stack">
-  </section>
+  <section class="grid-stack"></section>
 </template>
 
 <script lang="ts">
-import { EnvironmentOutlined, PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined, DashboardOutlined } from "@ant-design/icons-vue";
-import { defineComponent, onMounted, ref } from "vue";
-import { GridStack, GridStackNode } from 'gridstack';
-import barChartData from '@/types/chart/bar-chart';
-import lineChartData from '@/types/chart/line-chart';
-import bubbleChartData from '@/types/chart/bubble-chart';
-import doughnutPieChartData from '@/types/chart/doughnut-pie-chart';
-import polarAreaChartData from '@/types/chart/polar-chart';
-import radarChartData from '@/types/chart/radar-chart';
-import scatterChartData from '@/types/chart/scatter-chart';
-import gaugeChartData from '@/types/chart/gauge-chart';
+import {
+  EnvironmentOutlined,
+  PieChartTwoTone,
+  PieChartFilled,
+  PlusOutlined,
+  BarChartOutlined,
+  LineChartOutlined,
+  DotChartOutlined,
+  PieChartOutlined,
+  RadarChartOutlined,
+  DashboardOutlined,
+} from "@ant-design/icons-vue";
+import { defineComponent, onBeforeMount, onMounted, reactive, ref } from "vue";
+import { GridStack, GridStackNode } from "gridstack";
+import barChartData from "@/types/chart/bar-chart";
+import lineChartData from "@/types/chart/line-chart";
+import bubbleChartData from "@/types/chart/bubble-chart";
+import doughnutPieChartData from "@/types/chart/doughnut-pie-chart";
+import polarAreaChartData from "@/types/chart/polar-chart";
+import radarChartData from "@/types/chart/radar-chart";
+import scatterChartData from "@/types/chart/scatter-chart";
+import gaugeChartData from "@/types/chart/gauge-chart";
 import { useWidgetStore } from "@/store/widgets/widget";
-import uuid from '@/types/uuid'
+import uuid from "@/types/uuid";
+import { EWidget } from "@/types";
+import { SelectProps } from "ant-design-vue";
+import { useDashboardManagementStore } from "@/store";
+import { storeToRefs } from "pinia";
+import { useRoute } from "vue-router";
+
+interface FormState {
+  topicId: string;
+}
 
 export default defineComponent({
-  name: 'Dashboard',
-  components: { EnvironmentOutlined, DashboardOutlined, PieChartTwoTone, PieChartFilled, PlusOutlined, BarChartOutlined, LineChartOutlined, DotChartOutlined, PieChartOutlined, RadarChartOutlined },
-  setup () {
-    const useWidget = useWidgetStore()
+  name: "Dashboard",
+  components: {
+    EnvironmentOutlined,
+    DashboardOutlined,
+    PieChartTwoTone,
+    PieChartFilled,
+    PlusOutlined,
+    BarChartOutlined,
+    LineChartOutlined,
+    DotChartOutlined,
+    PieChartOutlined,
+    RadarChartOutlined,
+  },
+  setup() {
+    const useWidget = useWidgetStore();
     const visible = ref<boolean>(false);
+
+    // Dashboard Data
+    const storeDashboard = useDashboardManagementStore();
+    const { dataFull } = storeToRefs(storeDashboard);
+
+    // Config Data
+    const configVisible = ref<boolean>(false);
+    const titleConfig = ref<string>("");
+    const widgetSelection = ref<string>("");
+    const options = ref<SelectProps["options"]>([]);
+
+    // Get Dashboard
+    const route = useRoute();
+    const dashboardId = route.params.dashboardId;
+
+    // On Create
+    const onFinish = (values: any) => {
+      console.log(values);
+      switch (widgetSelection.value) {
+        case EWidget.BAR: {
+          useWidget.addNewBarChart(grid, uuid(), barChartData);
+          break;
+        }
+        case EWidget.BUBBLE: {
+          useWidget.addNewBubbleChart(grid, uuid(), bubbleChartData);
+          break;
+        }
+        case EWidget.DOUGHNUT: {
+          useWidget.addNewDoughnutChart(
+            grid,
+            uuid(),
+            doughnutPieChartData("doughnut")
+          );
+          break;
+        }
+        case EWidget.PIE: {
+          useWidget.addNewPieChart(grid, uuid(), doughnutPieChartData("pie"));
+          break;
+        }
+        case EWidget.GAUGE: {
+          useWidget.addNewGaugeChart(grid, uuid(), gaugeChartData);
+          break;
+        }
+        case EWidget.LINE: {
+          useWidget.addNewLineChart(grid, uuid(), lineChartData);
+          break;
+        }
+        case EWidget.POLAR: {
+          useWidget.addNewPolarAreaChart(grid, uuid(), polarAreaChartData);
+          break;
+        }
+        case EWidget.RADAR: {
+          useWidget.addNewRadarChart(grid, uuid(), radarChartData);
+          break;
+        }
+        case EWidget.SCATTER: {
+          useWidget.addNewScatterChart(grid, uuid(), scatterChartData);
+          break;
+        }
+        case EWidget.MAPS: {
+          useWidget.addNewMaps(grid, uuid());
+          break;
+        }
+      }
+      configVisible.value = false;
+      visible.value = false;
+      formState.topicId = "";
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+      console.log("Failed:", errorInfo);
+    };
+
+    const formState = reactive<FormState>({
+      topicId: "",
+    });
+
+    const handleChange = (value: string) => {
+      console.log(`selected ${value}`);
+    };
+
+    const onConfigClose = () => {
+      formState.topicId = "";
+    };
+
+    const handleFocus = () => {
+      options.value = dataFull.value
+        .find((dashboard) => dashboard.id === dashboardId)
+        ?.devices?.map((device) => ({
+          label: device.name,
+          options: device.topics
+            ?.filter((topic) => topic.widgetType === widgetSelection.value)
+            .map((topic) => ({
+              value: topic.id,
+              label: topic.name,
+            })),
+        }));
+    };
 
     const showDrawer = () => {
       visible.value = true;
     };
 
-    const activeKey = ref(['1', '2']);
+    const activeKey = ref(["1", "2"]);
+
+    onBeforeMount(async () => {
+      await storeDashboard.getDashboardFullList();
+    });
 
     let info = ref("");
     let grid: GridStack;
@@ -125,74 +308,103 @@ export default defineComponent({
         float: false,
         cellHeight: "70px",
         minRow: 7,
-        removable: true
+        removable: true,
       });
 
-      grid.on('resizestop', function(event: Event, el: any) {
+      grid.on("resizestop", function (event: Event, el: any) {
         let node: GridStackNode = el.gridstackNode;
         console.log(node);
       });
 
-      grid.on('added', (event: Event, items: any) => {
-        items.forEach(function(node: GridStackNode) {
+      grid.on("added", (event: Event, items: any) => {
+        items.forEach(function (node: GridStackNode) {
           console.log(node);
         });
       });
 
       grid.on("removed", (event: Event, items: any) => {
-        items.forEach(function(node: GridStackNode) {
-          console.log(node)
+        items.forEach(function (node: GridStackNode) {
+          console.log(node);
         });
       });
 
       grid.on("dragstop", (_, element: any) => {
         const node = element.gridstackNode;
         info.value = `you just dragged node #${node.id} to ${node.x},${node.y} – good job!`;
-        console.log(node)
+        console.log(node);
       });
     });
 
     function addNewMaps() {
-      useWidget.addNewMaps(grid, uuid())
+      configVisible.value = true;
+      titleConfig.value = "Maps";
+      widgetSelection.value = EWidget.MAPS;
     }
 
     function addNewGaugeChart() {
-      useWidget.addNewGaugeChart(grid, uuid(), gaugeChartData)
+      configVisible.value = true;
+      titleConfig.value = "Gauge Chart";
+      widgetSelection.value = EWidget.GAUGE;
     }
 
     function addNewScatterChart() {
-      useWidget.addNewScatterChart(grid, uuid(), scatterChartData)
+      configVisible.value = true;
+      titleConfig.value = "Scatter Chart";
+      widgetSelection.value = EWidget.SCATTER;
     }
 
     function addNewRadarChart() {
-      useWidget.addNewRadarChart(grid, uuid(), radarChartData)
+      configVisible.value = true;
+      titleConfig.value = "Radar Chart";
+      widgetSelection.value = EWidget.RADAR;
     }
 
     function addNewPolarAreaChart() {
-      useWidget.addNewPolarAreaChart(grid, uuid(), polarAreaChartData)
+      configVisible.value = true;
+      titleConfig.value = "Polar Chart";
+      widgetSelection.value = EWidget.POLAR;
     }
 
     function addNewPieChart() {
-      useWidget.addNewPieChart(grid, uuid(), doughnutPieChartData('pie'))
+      configVisible.value = true;
+      titleConfig.value = "Pie Chart";
+      widgetSelection.value = EWidget.PIE;
     }
 
     function addNewBubbleChart() {
-      useWidget.addNewBubbleChart(grid, uuid(), bubbleChartData)
+      configVisible.value = true;
+      titleConfig.value = "Bubble Chart";
+      widgetSelection.value = EWidget.BUBBLE;
     }
 
     function addNewLineChart() {
-      useWidget.addNewLineChart(grid, uuid(), lineChartData)
+      configVisible.value = true;
+      titleConfig.value = "Line Chart";
+      widgetSelection.value = EWidget.LINE;
     }
 
     function addNewBarChart() {
-      useWidget.addNewBarChart(grid, uuid(), barChartData)
+      configVisible.value = true;
+      titleConfig.value = "Bar Chart";
+      widgetSelection.value = EWidget.BAR;
     }
 
     function addNewDoughnutChart() {
-      useWidget.addNewDoughnutChart(grid, uuid(), doughnutPieChartData('doughnut'))
+      configVisible.value = true;
+      titleConfig.value = "Doughnut Chart";
+      widgetSelection.value = EWidget.DOUGHNUT;
     }
 
     return {
+      onConfigClose,
+      handleFocus,
+      onFinishFailed,
+      onFinish,
+      formState,
+      handleChange,
+      options,
+      titleConfig,
+      configVisible,
       info,
       addNewBarChart,
       addNewLineChart,
@@ -206,8 +418,8 @@ export default defineComponent({
       addNewMaps,
       visible,
       showDrawer,
-      activeKey
+      activeKey,
     };
-  }
-})
+  },
+});
 </script>
