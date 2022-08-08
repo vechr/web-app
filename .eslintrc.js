9d90786c06772690eae074e05cd5c766917c807f
zulfikar4568 <isnaen70@gmail.com>
2022-08-08 22:54:41 +0700

    :bug: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,18 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": 0,
-    "@typescript-eslint/no-explicit-any": "off",
-    quotes: ["error", "single"],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    quotes: ['error', 'single'],
   },
 };
