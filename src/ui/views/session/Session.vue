579be47e09b5b3ccfcfec54cbba0879ab1fd2af6
zulfikar4568 <isnaen70@gmail.com>
2022-10-15 18:02:10 +0700

    feat(session): feature remember me

--- src/ui/views/session/Session.vue
+++ src/ui/views/session/Session.vue
@@ -45,6 +45,10 @@
           </a-input-password>
         </a-form-item>
 
+        <a-form-item name="remember">
+          <a-checkbox v-model:checked="remember">Remember me</a-checkbox>
+        </a-form-item>
+
         <a-form-item>
           <a-button
             class="btn-auth"
@@ -61,13 +65,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent, onBeforeMount, reactive, ref } from 'vue';
 import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
+import { useCookies } from 'vue3-cookies';
 import { useCommonStore, useSessionStore } from '@/ui/store';
+import {
+  decryptedDataString,
+  encryptedDDataString,
+} from '@/utils/security.util';
 
 interface FormState {
   username: string;
   password: string;
+}
+
+interface CookieData {
+  secureUser: string;
+  securePass: string;
+  remember: boolean;
 }
 
 export default defineComponent({
@@ -77,19 +92,48 @@ export default defineComponent({
     LockOutlined,
   },
   setup() {
+    const remember = ref<boolean>(false);
     const common = useCommonStore();
     const store = useSessionStore();
+    const { cookies } = useCookies();
+
+    const dataCookie = reactive<CookieData>({
+      secureUser: '',
+      securePass: '',
+      remember: true,
+    });
 
     const formState = reactive<FormState>({
       username: '',
       password: '',
     });
+
+    onBeforeMount(() => {
+      dataCookie.secureUser = cookies.get('user-sec');
+      dataCookie.securePass = cookies.get('pass-sec');
+      dataCookie.remember = Boolean(cookies.get('remember'));
+
+      formState.username = decryptedDataString(dataCookie.secureUser);
+      formState.password = decryptedDataString(dataCookie.securePass);
+      remember.value = dataCookie.remember;
+    });
+
     const onFinish = (values: FormState) => {
       common.setIsLoading(true);
       store.login(values);
+      if (remember.value) {
+        cookies.set('user-sec', encryptedDDataString(values.username));
+        cookies.set('pass-sec', encryptedDDataString(values.password));
+        cookies.set('remember', String(remember.value));
+      } else {
+        cookies.remove('user-sec');
+        cookies.remove('pass-sec');
+        cookies.remove('remember');
+      }
     };
 
     return {
+      remember,
       formState,
       onFinish,
     };
